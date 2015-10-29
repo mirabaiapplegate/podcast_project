dc5a0c29bcc5043f30fd434b207282860716ecf1
Mirabai Applegate <mirabaiapplegate@Mirabais-MacBook-Pro.local>
2015-10-29 16:37:45 -0700

    Resources rendering through variables in db. Start_at not yet functional

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, redirect, request, flash, session
+from flask_bootstrap import Bootstrap
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import Podcast, Event, connect_to_db, db
