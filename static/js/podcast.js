8bf003fb6c57215ca278f7c04200eba42afea300
Mirabai Applegate <mirabaiapplegate@Mirabais-MacBook-Pro.local>
2015-11-02 15:16:53 -0800

    Add amplitudejs and css directories to static directory to make Flask happy

--- static/js/podcast.js
+++ static/js/podcast.js
@@ -2,6 +2,21 @@
 
 // Wait until page has loaded
 $(document).ready(function() {
+
+Amplitude.init({
+    "songs": [
+        {
+            "name": "Planet Money",
+            "podcast": "planet_money_659",
+            "album": "Rooms For Adelaide",
+            "url": "https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/659.mp3",
+            "live": false,
+            "cover_art_url": "https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/planet_money_659.jpg"
+        }
+    ]
+});
+
+
     var carousel_images = ['https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/planet_money_659.jpg', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/MessyAdvancedHoneybadger.gif', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/money-printing-press.jpg', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/money_shredder.jpg'];
  
       for(var i=0 ; i< carousel_images.length ; i++) {
