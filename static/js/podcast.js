62514b8056f3735e972222bc5790a856c5a4a5c5
Mirabai Applegate <mirabaiapplegate@Mirabais-MacBook-Pro.local>
2015-11-02 17:11:44 -0800

    Center images in Carousel

--- static/js/podcast.js
+++ static/js/podcast.js
@@ -31,14 +31,14 @@ $(document).ready(function() {
     var carousel_images = ['https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/planet_money_659.jpg', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/MessyAdvancedHoneybadger.gif', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/money-printing-press.jpg', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/money_shredder.jpg'];
  
       for(var i=0 ; i< carousel_images.length ; i++) {
-        $('<div class="item"><img src="'+carousel_images[i]+'"><div class="carousel-caption"></div></div>').appendTo('.carousel-inner');
+        $('<div class="item"><img src="'+carousel_images[i]+'" class="img-responsive center-block"><div class="carousel-caption"></div></div>').appendTo('.carousel-inner');
         
-        $('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+        $('<li data-target="#carousel-image" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
 
       }
       $('.item').first().addClass('active');
       $('.carousel-indicators > li').first().addClass('active');
-      $('#carousel-example-generic').carousel();
+      $('#carousel-image').carousel();
 
 
 });
