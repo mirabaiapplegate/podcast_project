a4ebabe4ec227ff94cf280894229d22f9ffaa3cf
Mirabai Applegate <mirabaiapplegate@Mirabais-MacBook-Pro.local>
2015-11-03 16:32:48 -0800

    Added Jinja to audio player so all podcasts will play

--- static/js/podcast.js
+++ static/js/podcast.js
@@ -3,19 +3,6 @@
 // Wait until page has loaded
 $(document).ready(function() {
 
-  // Initialize Amplitude Audio Player
-  Amplitude.init({
-    "songs": [
-        {
-            "name": "Planet Money",
-            "artist": "planet_money_659",
-            "url": "https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/659.mp3",
-            "live": false,
-            "cover_art_url": "https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/planet_money_659.jpg"
-        }
-    ],
-    "default_album_art": "images/no-cover.png"
-});
     /*
       jQuery Visual Helpers for Audio Player
     */
