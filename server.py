ff73aa883b283f67e92faef9299151ff9bf382b7
Mirabai Applegate <mirabaiapplegate@Mirabais-MacBook-Pro.local>
2015-11-03 17:54:08 -0800

    Added NPR API call to server

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, request, flash, session, json, jsonify
+from flask import Flask, render_template, redirect, request, flash, session, json, requests
 from flask_bootstrap import Bootstrap
 from flask_debugtoolbar import DebugToolbarExtension
 from json import dumps
@@ -51,6 +51,32 @@ def podcast(podcast_id):
     images = jsonify(images)
 
     return render_template("podcast.html", events=events, podcast=podcast, images=images)
+
+@app.route('/planet_money')
+def planet_money():
+    """ API call to NPR Planet Money to get 10 most recent episodes """
+    url = 'http://api.npr.org/query?apiKey=MDIwOTI5MjIyMDE0NDUzODQ0NzA3MTJmMw000&numResults=10&format=json&id=94427042&requiredAssets=audio&requiredAssets=text&requiredAssets=image' 
+
+    r = requests.get(url)
+    jdict = r.json()
+
+    for story in jdict['list']['story']:
+        title = story['title']['$text'] + "\n"
+
+        description = story['teaser']['$text'] + "\n"
+
+        if 'show' in story:
+            program = story['show'][0]['program']['$text'] + "\n"
+        
+        image = story['image'][0]['src'] + "\n"
+        
+        if 'caption' in story:
+            image_caption = story['image'][0]['caption']['$text'] + "\n"
+        
+        audio = story['audio'][0]['format']['mp3'][0]['$text'] + "\n" 
+        # TODO add to db...
+        new Podcast = sqlalchemy thing   
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
