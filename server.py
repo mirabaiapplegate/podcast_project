df4cc0994641974cac1d6a2effd8240aa49d0790
Mirabai Applegate <mirabaiapplegate@Mirabais-MacBook-Pro.local>
2015-11-03 16:21:49 -0800

    Moved carousel functionality to podcast.html because of variable troubles.

--- server.py
+++ server.py
@@ -2,12 +2,12 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, json, jsonify
 from flask_bootstrap import Bootstrap
 from flask_debugtoolbar import DebugToolbarExtension
-
+from json import dumps
 from model import Podcast, Event, connect_to_db, db
-import json
+
 
 app = Flask(__name__)
 
@@ -27,17 +27,30 @@ def index():
     return render_template("index.html", podcasts=podcasts)
 
 
+@app.route('/images/<int:podcast_id>.json')
+def work(podcast_id):
+
+    carousel_images = db.session.query(Event.url).filter(Event.podcast_id==podcast_id).all()
+   
+    images = {"urls": carousel_images }
+    print images
+
+    return jsonify(images)
+
+
 @app.route('/<int:podcast_id>')
 def podcast(podcast_id):
     """Show podcast user has selected"""
 
     events = Event.query.filter(Event.podcast_id==podcast_id)
     podcast = Podcast.query.get(podcast_id)
-
     carousel_images = db.session.query(Event.url).filter(Event.podcast_id==podcast_id).all()
-    print carousel_images
-    return render_template("podcast.html", events=events, podcast=podcast,
-                            carousel_images=map(json.dumps, carousel_images))
+    
+    images = {podcast_id: carousel_images }
+
+    images = jsonify(images)
+
+    return render_template("podcast.html", events=events, podcast=podcast, images=images)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
--- static/js/podcast.js
+++ static/js/podcast.js
@@ -2,6 +2,8 @@
 
 // Wait until page has loaded
 $(document).ready(function() {
+
+  // Initialize Amplitude Audio Player
   Amplitude.init({
     "songs": [
         {
@@ -14,9 +16,8 @@ $(document).ready(function() {
     ],
     "default_album_art": "images/no-cover.png"
 });
-
     /*
-      jQuery Visual Helpers
+      jQuery Visual Helpers for Audio Player
     */
     $('#small-player').hover(function(){
       $('#small-player-middle-controls').show();
@@ -26,19 +27,5 @@ $(document).ready(function() {
       $('#small-player-middle-meta').show();
 
     });
-
-
-    var carousel_images = ['https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/planet_money_659.jpg', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/MessyAdvancedHoneybadger.gif', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/money-printing-press.jpg', 'https://s3-us-west-2.amazonaws.com/hbpodcastproject/planet_money_659/money_shredder.jpg'];
- 
-      for(var i=0 ; i< carousel_images.length ; i++) {
-        $('<div class="item"><img src="'+carousel_images[i]+'" class="img-responsive center-block"><div class="carousel-caption"></div></div>').appendTo('.carousel-inner');
-        
-        $('<li data-target="#carousel-image" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
-
-      }
-      $('.item').first().addClass('active');
-      $('.carousel-indicators > li').first().addClass('active');
-      $('#carousel-image').carousel();
-
 
 });
