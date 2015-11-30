9f7cb7ba579dd84bd3d185de784db218c4743e95
Mirabai <mirabai.applegate@gmail.com>
2015-11-29 16:29:58 -0800

    Add tests

---
+++ story_strata_tests.py
@@ -0,0 +1,26 @@
+import os
+from server import app
+from model import connect_to_db
+import unittest
+from flask import url_for
+import tempfile
+
+class StoryStrataTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+        connect_to_db(app)
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
+
+    def test_podcasts_index(self):
+        response = self.app.get('/podcasts')
+        assert 'Podcast' in response.data
+
+
+if __name__ == '__main__':
+    unittest.main()
